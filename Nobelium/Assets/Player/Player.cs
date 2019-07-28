88fc225861f0ca6a4c11fb24e73d62928461f92d
misha <homakhaker@gmail.com>
2019-07-28 14:53:53 +0100

    Created movementComponent with basic movement

--- Nobelium/Assets/Player/Player.cs
+++ Nobelium/Assets/Player/Player.cs
@@ -7,7 +7,33 @@ public class Player : MonoBehaviour
     [Header("Inputs")]
     [SerializeField] private float horInput;
     [SerializeField] private float verInput;
-   
+
+
+    [Header("Stats")]
+    [SerializeField] private float speed;
+
+
+    private Rigidbody2D rb2d;
+
+    private MovementComponent movementComponent;
+
+
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        createComponents();
+    }
+
+    private void createComponents()
+    {
+        movementComponent = new MovementComponent(rb2d);
+    }
+
+    private void FixedUpdate()
+    {
+        movementComponent.movement(horInput);
+    }
 
     public void setInputs(float horInput, float verInput)
     {
