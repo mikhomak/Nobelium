e39d02a8c1df4dd3188c08c6b02feb573346a2aa
misha <homakhaker@gmail.com>
2019-07-28 18:33:41 +0100

    Added auidoPeer

---
+++ Nobelium/Assets/Auido Visualization/AudioPeer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+
+    [SerializeField] private float[] samples = new float[512];
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        getSpectrumAudioSource();
+    }
+
+    private void getSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples,0,FFTWindow.Blackman);
+    }
+
+}
