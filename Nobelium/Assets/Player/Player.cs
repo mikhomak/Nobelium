d3d79bfbd21985ce5ca8889b8b675d5ba87db260
misha <homakhaker@gmail.com>
2019-07-28 14:43:53 +0100

    Created InputComponent with horizontal and vertical input

---
+++ Nobelium/Assets/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Inputs")]
+    [SerializeField] private float horInput;
+    [SerializeField] private float verInput;
+   
+
+    public void setInputs(float horInput, float verInput)
+    {
+        this.horInput = horInput;
+        this.verInput = verInput;
+    }
+
+}
