9fab3818376054f01c94ecb4487fe000c22ead7c
misha <homakhaker@gmail.com>
2019-08-18 18:44:16 +0100

    Added boxes for boss

---
+++ Nobelium/Assets/Components/Hitbox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    [SerializeField] private float actualDamage;
+    [SerializeField] private float minScale = 0.1f;
+    [SerializeField] private float maxScale = 0.5f;
+    [SerializeField] private float lifeTime = 2f;
+
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void FixedUpdate()
+    {
+        float scale = CommonMethods.getValueInRange(AudioPeer.getAudioBandBuffer(5), minScale, maxScale);
+        transform.localScale = new Vector2(scale,scale);
+        actualDamage = scale * damage;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == CommonMethods.HURTBOX)
+        {
+            collision.GetComponent<IHurtbox>().takeDamage(actualDamage);
+        }
+    }
+
+}
--- Nobelium/Assets/Enemy/Enemy.cs
+++ Nobelium/Assets/Enemy/Enemy.cs
@@ -2,12 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour, ICharacter
 {
     [Header("Stats")]
     [SerializeField] private float speed;
     [SerializeField] private float health;
+    [Header("References")]
+    [SerializeField] private GameObject boxPrefab;
+    [SerializeField] private BoxPoints boxPoints;
+
+    [Header("Cooldowns")]
+    [SerializeField] private float boxCooldown = 6f;
+    [SerializeField] private float boxTimer = 0f;
+
+
     private Rigidbody2D rb2d;
 
 
@@ -19,6 +29,10 @@ public class Enemy : MonoBehaviour, ICharacter
         rb2d = GetComponent<Rigidbody2D>();
         createComponents();
         setStats();
+    }
+
+    private void FixedUpdate() {
+        createBoxes();
     }
 
     private void createComponents()
@@ -33,7 +47,23 @@ public class Enemy : MonoBehaviour, ICharacter
         healthComponent.setHealth(health);
     }
 
-    
+
+    private void createBoxes() {
+        if (AudioPeer.getAudioBandBuffer(0) > 0.1f) {
+            if (boxTimer > boxCooldown)
+            {
+                int random = Random.Range(0,boxPoints.getMaxPoints()-1);
+                boxPoints.updatePointsTaken();
+                for(int i = 0; i < random; i++)
+                {
+                    Instantiate(boxPrefab, boxPoints.getRandomPosition(), transform.rotation);
+                }
+                boxTimer = 0f;
+            }
+        }
+        boxTimer += Time.deltaTime;
+    }
+
 
     public void die()
     {
